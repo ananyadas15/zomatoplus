8cd2155e4fb39a634d004e43b8966a8e87f76eca
ananyadas15 <ananya15.das@gmail.com>
2018-06-29 01:21:35 +0530

    Integrated Restaurant, Item and User APIs with AngularJS

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,146 @@
+
+var app = angular.module("app", [ "ngRoute" ]);
+
+app.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/home', {
+		templateUrl : 'home.html'
+	});
+	$routeProvider.when('/user', {
+		templateUrl : 'user.html',
+		controller : 'userCtrl'
+	});
+	$routeProvider.when('/restaurants', {
+		templateUrl : 'restaurant.html',
+		controller : 'restaurantCtrl'
+	});
+	$routeProvider.when('/items', {
+		templateUrl : 'items.html',
+		controller : 'itemsCtrl'
+	});
+	$routeProvider.otherwise({
+		redirectTo : '/home'
+	});
+} ]);
+
+
+
+app.controller("restaurantCtrl", function($scope, $http) {
+
+	$scope.fetchRestaurant = function() {
+		$http({
+			method : 'GET',
+			url : 'http://localhost:9080/restaurant/getAll'
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.status = status;
+			$scope.restaurants = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+	
+	
+	$scope.saveRestaurant = function() {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:9080/restaurant/add',
+			headers: { 'Content-Type': 'application/json' },
+			data:$scope.restaurant
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.fetchRestaurant();
+			$scope.restaurants = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+
+});
+
+app.controller("itemsCtrl", function($scope, $http) {
+
+	$scope.saveItem = function() {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:9080/restaurant/addItems',
+			headers: { 'Content-Type': 'application/json' },
+			data:$scope.restaurant
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.fetchRestaurant();
+			$scope.restaurants = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+		
+	$scope.fetchItem = function() {
+			$http({
+				method : 'GET',
+				url : 'http://localhost:9080/restaurant/showItems'
+			}).success(function(data, status) {
+				console.log(data);
+				$scope.status = status;
+				$scope.restaurants = data;
+			}).error(function(data, status) {
+				$scope.status = status;
+				$scope.data = "Request failed";
+			});
+		};
+
+})
+
+
+app.controller("userCtrl", function($scope, $http) {
+
+	$scope.signup = function() {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:9080/user/add',
+			headers: { 'Content-Type': 'application/json' },
+			data:$scope.restaurant
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.fetchRestaurant();
+			$scope.restaurants = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+	
+	
+	$scope.login = function() {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:9080/user/login',
+			headers: { 'Content-Type': 'application/json' },
+			data:$scope.restaurant
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.fetchRestaurant();
+			$scope.restaurants = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+	
+	$scope.fetchUser = function() {
+		$http({
+			method : 'GET',
+			url : 'http://localhost:9080/user/getAll'
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.status = status;
+			$scope.restaurants = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+
+});
